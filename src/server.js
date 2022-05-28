6b68ab412213b6f4c81723fe65ab2bf8cab953e0
Tale152 <alessandro.talmi@studio.unibo.it>
2022-05-28 04:44:15 +0200

    added line to use json body parser

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const server = express()
 server.use(cors())
+server.use(bodyParser.json())
 
-server.get('/example', require('./routes/example.get.js').example)
+server.post('/push-job', require('./routes/post.push-job').pushJob)
 
 module.exports = server
